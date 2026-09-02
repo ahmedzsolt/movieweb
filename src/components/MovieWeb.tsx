df7e04066ac9345a4ab14a19e29aeb0bc89610c4
Ahmed Zsolt Spiák Dakroub <ahmedzsolt@gmail.com>
2026-09-02 09:27:18 +0200

    Add loading feature

--- src/components/MovieWeb.tsx
+++ src/components/MovieWeb.tsx
@@ -4,15 +4,19 @@ import { getRecommendations } from "../services/tmdb";
 
 function MovieWeb({chosenMovies, setChosenMovies}) {
     const [recommendedMovies, setRecommendedMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     function getChosenMovie() {
         return chosenMovies[chosenMovies.length - 1];
     }
 
     useEffect(() => {
+        setIsLoading(true);
+
         async function populateRecommendedMovies() {
             const movies = await getRecommendations(getChosenMovie().id);
             setRecommendedMovies(movies);
+            setIsLoading(false);
         }
 
         populateRecommendedMovies();
@@ -22,12 +26,20 @@ function MovieWeb({chosenMovies, setChosenMovies}) {
     return (
         <div>
             <button onClick={() => setChosenMovies([])}>Start over</button>
-            <p>{getChosenMovie().title}</p>
-            <h2>Recommendations:</h2>
+
             {
-                recommendedMovies.map(movie => (
-                    <button key={movie.id} onClick={() => setChosenMovies(prev => [...prev, movie])}>{movie.title}</button>
-                ))
+                isLoading ? (<p>Data is loaing...</p>) : 
+                (
+                    <>
+                        <p>{getChosenMovie().title}</p>
+                        <h2>Recommendations:</h2>
+                        {
+                            recommendedMovies.map(movie => (
+                                <button key={movie.id} onClick={() => setChosenMovies(prev => [...prev, movie])}>{movie.title}</button>
+                            ))
+                        }
+                    </>
+                )
             }
         </div>
     );
