11fae57cc8ddd0962707330db30afc4a67869ffa
Ahmed Zsolt Spiák Dakroub <ahmedzsolt@gmail.com>
2026-09-05 15:22:58 +0200

    Add select at least one streaming provider

--- src/components/MovieSearch.tsx
+++ src/components/MovieSearch.tsx
@@ -12,6 +12,20 @@ function MovieSearch() {
 
     const {chosenMovies, setChosenMovies, providers, setProviders}: ChosenMovieTypes = useContext(MovieContext);
 
+    function handleCheckbox(event: React.ChangeEvent<HTMLInputElement>) {
+        const name = event.target.name;
+        const checked = event.target.checked;
+        setProviders(prev => prev.map(provider => provider.name === name ? { ...provider, checked: checked} : provider));
+    }
+
+    function anyProviderSelected() {
+        if(providers.some(provider => provider.checked)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     useEffect(() => {
         if(searchInput.length < 3) {
             setSearchResults([]);
@@ -33,30 +47,27 @@ function MovieSearch() {
         return () => clearTimeout(timeout);
     }, [searchInput]);
 
-    function handleCheckbox(event: React.ChangeEvent<HTMLInputElement>) {
-        const name = event.target.name;
-        const checked = event.target.checked;
-        setProviders(prev => prev.map(provider => provider.name === name ? { ...provider, checked: checked} : provider));
-    }
-
     return(
         <div className="flow flow-col w-[90%] max-w-4xl">
             <h1 className="text-center text-4xl mt-20">Struggle to find a movie to watch?</h1>
             <p className="text-center mt-6 mb-10 text-base">Select a movie you like, and we will find you recommendations based on that movie.</p>
-            <div>
+            <div className="flex flex-row gap-x-3 mb-2 justify-center">
                 {
                     providers.map(provider => (
-                        <label key={provider.provider_id}>
-                            <input type="checkbox" name={provider.name} checked={!!provider.checked} onChange={handleCheckbox}/>
+                        <label className="text-sm" key={provider.provider_id}>
+                            <input type="checkbox" className="mr-1" name={provider.name} checked={!!provider.checked} onChange={handleCheckbox}/>
                             {provider.name}
                         </label>
                     ))
                 }
             </div>
-            <input type="text" placeholder="Type something..." value={searchInput} onChange={event => setSearchInput(event.target.value)} className="w-full rounded-2xl border border-white/10 bg-white/5 px-5 py-4 text-lg text-white placeholder-white/30 shadow-xl backdrop-blur outline-none transition-all focus:border-white/30 focus:bg-white/10 focus:ring-2 focus:ring-white/10" />
+            <div>
+                <p className={anyProviderSelected() ? 'invisible' : 'visible text-red-500 text-sm mb-2'}>You must select at least one streaming service.</p>
+            </div>
+            <input type="text" placeholder="Type something..." disabled={anyProviderSelected() ? '' : 'disabled text-gray-100'} value={searchInput} onChange={event => setSearchInput(event.target.value)} className="w-full rounded-2xl border border-white/10 bg-white/5 px-5 py-4 text-lg text-white placeholder-white/30 shadow-xl backdrop-blur outline-none transition-all focus:border-white/30 focus:bg-white/10 focus:ring-2 focus:ring-white/10" />
             <div className="pt-4 px-4">
                 {
-                    !isLoadingSearchResults ? (
+                    !isLoadingSearchResults && anyProviderSelected() ? (
                         searchResults.map(movie => (
                             <button className="flex flex-row gap-x-2 mb-2 cursor-pointer text-left" key={movie.id} onClick={() => setChosenMovies(prev => [...prev, movie])}>
                                 
